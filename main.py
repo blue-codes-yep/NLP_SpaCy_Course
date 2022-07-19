17168a369f31aa883219199d8b3dc942230c9496
linzblue1 <linzblue5@gmail.com>
2022-07-19 11:32:08 -0500

    Working more with spaCy, getting a basic understanding.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import spacy
+from spacy import displacy
 
 nlp = spacy.load("en_core_web_sm")
 
-print(nlp)
+with open("data/wiki_us.txt", "r") as f:
+    text = f.read()
+
+doc = nlp(text)
+
+# Displays a cool little graph showing how the words, connect throughout the sentence.
+'''
+text = "Hi, my name is Blue, and this is pretty cool I like seeing cool stuff like this!"
+doc2 = nlp(text)
+
+
+'''
+
+# for ent in doc.ents:
+#    print(ent.text, ent.label_)
+
+displacy.serve(doc, style="ent")
